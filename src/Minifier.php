f690b869280351a71c007fd30cbb17a5d8545fba
mgralikowski <mgralikowski@gmail.com>
2019-08-02 00:58:04 +0200

    Update Minifier.php
    Disable (cause problems with spans).
    Todo: make exceptions.

--- src/Minifier.php
+++ src/Minifier.php
@@ -12,7 +12,7 @@ class Minifier
         // Shorten multiple white spaces
         '/\s{2,}/' => ' ',
         // Remove whitespaces between HTML tags
-        '/>\s{2,}</' => '><',
+        // '/>\s{2,}</' => '><',
         // Collapse new lines
         '/(\r?\n)/' => '',
     ];
